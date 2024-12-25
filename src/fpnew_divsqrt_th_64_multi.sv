b9732465ac61c5fbe412767b78c9e37d67253397
c_yongheng <1358528729@qq.com>
2024-12-25 10:38:29 +0800

    fix BUG: flush BUG when using THMULTI divsqrt unit

--- src/fpnew_divsqrt_th_64_multi.sv
+++ src/fpnew_divsqrt_th_64_multi.sv
@@ -373,11 +373,11 @@ module fpnew_divsqrt_th_64_multi #(
   
   // Select func 1 cycle after div issue
   logic func_sel;
-  `FFLARNC(func_sel, 1'b1, op_starting, func_sel, 1'b0, clk_i, rst_ni)
+  `FFLARNC(func_sel, 1'b1, op_starting, (func_sel|flush_i), 1'b0, clk_i, rst_ni)
 
   // Select operands 2 cycles after div issue
   logic op_sel;
-  `FFLARNC(op_sel, 1'b1, func_sel, op_sel, 1'b0, clk_i, rst_ni)
+  `FFLARNC(op_sel, 1'b1, func_sel, (op_sel|flush_i), 1'b0, clk_i, rst_ni)
 
   ct_vfdsu_top i_ct_vfdsu_top (
     .cp0_vfpu_icg_en                ( 1'b0                      ), // Internal clock gating, (module enable) doesn't matter when the clk_gate module is redundant anyway
